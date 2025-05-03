c8519f2ae2daec34083ee0d3c333cb6410506a2b
mtalaei67 <129692407+mtalaei67@users.noreply.github.com>
2025-05-03 16:14:13 +0330

    Create api_integration.php

---
+++ ai_articles_wp-plugin/api_integration.php
@@ -0,0 +1,10 @@
+<?php
+
+function translate_content($content, $target_language = 'fa') {
+    // Placeholder for Hugging Face or other API integration
+    $translated_content = [
+        'title' => 'عنوان نمونه',
+        'text' => 'این محتوای ترجمه شده است.'
+    ];
+    return $translated_content;
+}
